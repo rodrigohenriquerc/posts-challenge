ff0fd15417ac763fbf50c65fb50bb88a10395b24
Rodrigo Carvalho <rhrc09@gmail.com>
2021-03-14 11:13:06 -0300

    feat: add posts module types

---
+++ src/data/store/modules/posts/types.ts
@@ -0,0 +1,30 @@
+import { Post } from "../../../../domains/posts/models/Post";
+import { GetPosts } from "../../../../domains/posts/usecases/GetPosts";
+
+export enum ActionTypes {
+  GET_POSTS_REQUEST = "GET_POSTS_REQUEST",
+  GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS",
+  GET_POSTS_FAILURE = "GET_POSTS_FAILURE",
+}
+
+export interface RequestAction {
+  type: ActionTypes.GET_POSTS_REQUEST;
+}
+
+export interface SuccessAction {
+  type: ActionTypes.GET_POSTS_SUCCESS;
+  payload: { data: GetPosts.Data };
+}
+
+export interface FailureAction {
+  type: ActionTypes.GET_POSTS_FAILURE;
+  payload: { error: string };
+}
+
+export type Action = RequestAction | SuccessAction | FailureAction;
+
+export interface PostsState {
+  data: Post[];
+  error: string | null;
+  isLoading: boolean;
+}
