165fb3d6b1e0ce6617948517f94858d498628e4a
Rodrigo Carvalho <rhrc09@gmail.com>
2021-03-14 18:01:05 -0300

    feat: add store to provider

--- src/main/App/index.tsx
+++ src/main/App/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import store from 'data/store'
 import { Posts } from 'presentation/pages'
 import 'presentation/styles/reset.css'
 import 'presentation/styles/global.css'
 
-const App: React.FC = () => <Posts />
+const App: React.FC = () => (
+  <Provider store={store}>
+    <Posts />
+  </Provider>
+)
 
 export default App
