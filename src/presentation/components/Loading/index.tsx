9498a249cfc212fe559b3721c292d37bae95334d
Rodrigo Carvalho <rhrc09@gmail.com>
2021-03-14 19:16:41 -0300

    feat: add Loading component

---
+++ src/presentation/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as S from './styles'
+
+const Loading: React.FC = () => (
+  <S.Container data-testid="loading-ui">
+    <div />
+    <div />
+    <div />
+    <div />
+  </S.Container>
+)
+
+export default Loading
