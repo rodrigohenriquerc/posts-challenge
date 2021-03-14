11b6a348d38ab2133beb6070dea068cd72492c39
Rodrigo Carvalho <rhrc09@gmail.com>
2021-03-14 18:00:12 -0300

    fix: fix sagas return type problem

--- src/data/store/modules/posts/sagas/index.ts
+++ src/data/store/modules/posts/sagas/index.ts
@@ -2,12 +2,13 @@ import { all, takeLatest, put, call } from 'redux-saga/effects'
 import { ActionTypes } from '../types'
 import * as Actions from '../actions'
 import { httpClient } from 'infra/http/httpClient'
-import { getPosts } from 'data/http/services'
+import { Data, getPosts } from 'data/http/services'
 
-export function* GetPostsSaga() {
+export function* GetPostsSaga(): Generator<any, unknown, any> {
   try {
     const data = yield call(getPosts, httpClient)
     yield put(Actions.GetPostsSuccess(data))
+    return undefined
   } catch (error) {
     yield put(Actions.GetPostsFailure(error.message))
   }
--- src/data/store/modules/rootSaga.ts
+++ src/data/store/modules/rootSaga.ts
@@ -3,6 +3,6 @@ import { all } from 'redux-saga/effects'
 
 import postsSagas from './posts/sagas'
 
-export default function* rootSaga() {
+export default function* rootSaga(): Generator<any, unknown, any> {
   return yield all([postsSagas])
 }
