1e0f57697b57e8d9b8108a9adcac6685e17b7607
Rodrigo Carvalho <rhrc09@gmail.com>
2021-03-14 13:22:03 -0300

    chore: disable unnecessary eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  extends: ["airbnb", "airbnb/hooks"],
-};
+  extends: ['airbnb', 'airbnb/hooks'],
+  rules: {
+    semi: 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'react/jsx-filename-extension': 0,
+    'comma-dangle': 0,
+    'arrow-body-style': 0,
+    'import/prefer-default-export': 0,
+    'object-curly-newline': 0,
+  },
+}
