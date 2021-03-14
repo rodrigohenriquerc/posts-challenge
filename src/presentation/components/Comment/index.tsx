c196517bcbd30e964fb6e26a3ea64e253b4cc9e3
Rodrigo Carvalho <rhrc09@gmail.com>
2021-03-14 20:06:38 -0300

    test: ensure Comment renders with right author and description

--- src/presentation/components/Comment/index.tsx
+++ src/presentation/components/Comment/index.tsx
@@ -3,7 +3,7 @@ import { IComment } from './models'
 import * as S from './styles'
 
 const Comment: React.FC<IComment> = ({ author, description }: IComment) => (
-  <S.Container>
+  <S.Container data-testid="post-comment">
     <S.Author>{author}</S.Author>
     <S.Description>{description}</S.Description>
   </S.Container>
