eeb76dfc1d35fbfb4842a4915b746119012008e7
Rodrigo Carvalho <rhrc09@gmail.com>
2021-03-15 01:09:38 -0300

    feat: add comments saga to rootSaga

--- src/data/store/modules/rootSaga.ts
+++ src/data/store/modules/rootSaga.ts
@@ -2,7 +2,8 @@
 import { all } from 'redux-saga/effects'
 
 import postsSagas from './posts/sagas'
+import commentsSagas from './comments/sagas'
 
 export default function* rootSaga(): Generator<any, unknown, any> {
-  return yield all([postsSagas])
+  return yield all([postsSagas, commentsSagas])
 }
