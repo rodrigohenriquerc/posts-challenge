0ebde56081e77fdb63833345fa4ecb3b2f46245b
Rodrigo Carvalho <rhrc09@gmail.com>
2021-03-15 01:07:12 -0300

    refactor: use data adapter in GetCommentsSaga

--- src/data/store/modules/comments/sagas/index.ts
+++ src/data/store/modules/comments/sagas/index.ts
@@ -2,22 +2,15 @@ import { all, takeLatest, put, call } from 'redux-saga/effects'
 import { ActionTypes, RequestAction } from '../types'
 import * as Actions from '../actions'
 import { httpClient } from 'infra/http/httpClient'
-import { getComments, GetCommentsProtocol } from 'data/http/services'
-import { GetComments } from 'domains/posts/usecases'
+import { getComments } from 'data/http/services'
+import { adaptData } from './Adapter'
 
 export function* GetCommentsSaga({
   payload,
 }: RequestAction): Generator<any, unknown, any> {
   try {
     const data = yield call(getComments, payload.params, httpClient)
-    const newData: GetComments.Data = data.map(
-      (item: GetCommentsProtocol.Comment) => ({
-        id: item.id,
-        author: item.name,
-        description: item.body,
-      })
-    )
-    yield put(Actions.GetCommentsSuccess(newData))
+    yield put(Actions.GetCommentsSuccess(adaptData(data)))
     return undefined
   } catch (error) {
     yield put(Actions.GetCommentsFailure(error.message))
